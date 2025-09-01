c50a65a923305a15a672a90c553903a23777694d
Victor Hugo <victorhugocordeirocha@gmail.com>
2025-09-01 14:02:59 -0400

    Criando o model Category e colocando o foreignkey

--- contact/admin.py
+++ contact/admin.py
@@ -7,9 +7,13 @@ from contact import models
 class ContactAdmin(admin.ModelAdmin):
     list_display = 'id', 'first_name', 'last_name', 'phone',
     ordering = '-id',
-    # list_filter = 'created_date',
     search_fields = 'id', 'first_name', 'last_name',
     list_per_page = 15
     list_max_show_all = 100
     list_editable = 'first_name', 'last_name',
     list_display_links = 'id', 'phone',
+
+@admin.register(models.Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = 'name',
+    ordering = '-id',
