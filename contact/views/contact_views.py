3d09b12392c6bb1ceaf13119dc4b0bfe946b4ffa
Victor Hugo <victorhugocordeirocha@gmail.com>
2025-09-02 15:32:37 -0400

    Criando arquivos parciais e add o titulo do site de forma dinamica

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -24,10 +24,11 @@ def contact(request, contact_id):
     #     raise Http404()
 
     single_contact = get_object_or_404(Contact.objects.filter(pk=contact_id, show=True))
-
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
 
     context = {
         'contact': single_contact,
+        'site_title': site_title
     }
 
     return render (
